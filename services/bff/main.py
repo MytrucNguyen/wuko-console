c476ae35be253154d16c4f3fb31fda8163057bb1
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2026-06-06 18:28:05 -0700

    feat(bff): allow CORS from localhost:3000

--- services/bff/main.py
+++ services/bff/main.py
@@ -1,12 +1,22 @@
 import os 
 import httpx
+
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
+
 
 
 load_dotenv()
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 DEVICE_SERVICE_URL = os.getenv("DEVICE_SERVICE_URL")
 
